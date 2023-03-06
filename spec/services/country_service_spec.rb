8459376e78e002732461e634a596631d33beaad4
Braxton Eddings <beddings81@gmail.com>
2023-03-06 10:33:56 -0600

    Complete country service to return capital city

--- spec/services/country_service_spec.rb
+++ spec/services/country_service_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe CountryService do
-  describe 'get_random_country' do
+  describe '#get_random_country' do
     it 'will return a random country name', :vcr do
       country = CountryService.get_random_country
 
@@ -12,6 +12,18 @@ RSpec.describe CountryService do
 
       expect(country[:name]).to have_key(:common)
       expect(country[:name][:common]).to be_a(String)
+
+    end
+  end
+
+  describe '#get_capital city' do
+    it 'will return the capital city of a country' do
+      country = CountryService.get_capital_city("France")
+
+      expect(country).to be_a(Hash)
+      expect(country).to have_key(:capital)
+      expect(country[:capital]).to be_a(Array)
+      expect(country[:capital][0]).to eq("Paris")
 
     end
   end
