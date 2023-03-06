c119a76c4e9e67601ab4384ee7227f8d7db56a83
Braxton Eddings <beddings81@gmail.com>
2023-03-06 10:52:57 -0600

    Complete country facade to create a capital poro

--- app/facades/country_facade.rb
+++ app/facades/country_facade.rb
@@ -1,4 +1,10 @@
 class CountryFacade
+
+  def self.get_capital_city(country)
+    capital = CountryService.get_capital_city(country)
+    Capital.new(capital)
+  end
+
   def self.get_random_country
     country = CountryService.get_random_country
     Country.new(country)
---
+++ app/poros/capital.rb
@@ -0,0 +1,9 @@
+class Capital
+  attr_reader :name, :lat, :lon
+
+  def initialize(data)
+    @name = data[:capital][0]
+    @lat = data[:capitalInfo][:latlng][0]
+    @lon = data[:capitalInfo][:latlng][1]
+  end
+end
