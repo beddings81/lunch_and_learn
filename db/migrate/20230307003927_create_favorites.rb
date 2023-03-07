50c68e8ce9c7f469336e8c844e245ea618e23524
Braxton Eddings <beddings81@gmail.com>
2023-03-06 18:59:40 -0600

    Create favorites and add user foreign key

---
+++ db/migrate/20230307003927_create_favorites.rb
@@ -0,0 +1,9 @@
+class CreateFavorites < ActiveRecord::Migration[5.2]
+  def change
+    create_table :favorites do |t|
+      t.string :country
+      t.string :recipe_link
+      t.string :recipe_title
+    end
+  end
+end
